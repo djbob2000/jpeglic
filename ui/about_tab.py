55ca0a372c34a7caeab1f0f1e2b86f30eacce49a
JacobDev1 <65915437+JacobDev1@users.noreply.github.com>
2024-09-13 12:08:55 +0200

    Improve about_tab layout

--- ui/about_tab.py
+++ ui/about_tab.py
@@ -5,6 +5,7 @@ from PySide6.QtWidgets import(
     QLabel,
     QPushButton,
     QSizePolicy,
+    QWidget,
 )
 from PySide6.QtCore import(
     Qt,
@@ -19,15 +20,17 @@ from data import fonts
 class AboutTab(QWidget):
     def __init__(self):
         super(AboutTab, self).__init__()
-
-        tab_lt = QHBoxLayout()
-        self.setLayout(tab_lt)
         self.update_checker = UpdateChecker()
 
-        # Label
-        title_l = QLabel(f"XL Converter")
-        version_l = QLabel(f"Version {VERSION}")
-        credits_l = QLabel(f"""
+        self.setupWidgets()
+        self.setupLayouts()
+        self.setupStyles()
+    
+    def setupWidgets(self):
+        # Labels
+        self.title_l = QLabel(f"XL Converter")
+        self.version_l = QLabel(f"Version {VERSION}")
+        self.credits_l = QLabel(f"""
             <div style='line-height: 120%;'>
                 <a href=\"https://codepoems.eu\">website</a>
                 <br>
@@ -37,50 +40,60 @@ class AboutTab(QWidget):
                 <a href=\"{QUrl.fromLocalFile(LICENSE_3RD_PARTY_PATH).toString()}\">3rd party</a>
             </div>
         """)
-        credits_l.linkActivated.connect(lambda qurl: openUrl(qurl))
+        self.credits_l.linkActivated.connect(lambda qurl: openUrl(qurl))
 
-        title_l.setOpenExternalLinks(True)
-        credits_l.setOpenExternalLinks(False)
-
-        ## Label - styles
-        title_l.setFont(fonts.ABOUT_TITLE)
-        version_l.setFont(fonts.ABOUT_VERSION)
-        credits_l.setFont(fonts.ABOUT_DESC)
-
-        title_l.setStyleSheet("padding-bottom: 3px;")
-        version_l.setStyleSheet("padding-bottom: 5px;")
-
-        title_l.setAlignment(Qt.AlignCenter)
-        version_l.setAlignment(Qt.AlignCenter)
-        credits_l.setAlignment(Qt.AlignCenter)
-        
-        text_vb = QVBoxLayout()
-        text_vb.addWidget(title_l)
-        text_vb.addWidget(version_l)
-        text_vb.addWidget(credits_l)
-        tab_lt.addLayout(text_vb)
+        self.title_l.setOpenExternalLinks(True)
+        self.credits_l.setOpenExternalLinks(False)
 
         # Buttons
-        buttons_vb = QVBoxLayout()
-
         self.update_btn = QPushButton("Check for Updates", clicked=self.checkForUpdate)
         self.update_checker.finished.connect(lambda: self.update_btn.setEnabled(True))
         self.manual_btn = QPushButton("Manual", clicked=lambda: openRemoteUrl("https://xl-docs.codepoems.eu/"))
         self.report_bug_btn = QPushButton("Report Bug", clicked=lambda: openRemoteUrl("https://github.com/JacobDev1/xl-converter/issues"))
         self.donate_btn = QPushButton("Donate", clicked=lambda: openRemoteUrl("https://codepoems.eu/donate"))
 
+    def setupLayouts(self):
+        # Labels
+        labels_vb = QVBoxLayout()
+        labels_vb.addWidget(self.title_l)
+        labels_vb.addWidget(self.version_l)
+        labels_vb.addWidget(self.credits_l)
+
+        # Buttons
+        buttons_vb = QVBoxLayout()
         buttons_vb.addWidget(self.update_btn)
         buttons_vb.addWidget(self.manual_btn)
         buttons_vb.addWidget(self.report_bug_btn)
         buttons_vb.addWidget(self.donate_btn)
-        tab_lt.addLayout(buttons_vb)
+        
+        # Main
+        self.content_w = QWidget()
+        self.content_lt = QHBoxLayout(self.content_w)
+        self.content_lt.addLayout(labels_vb)
+        self.content_lt.addLayout(buttons_vb)
 
-        # Layout
-        text_vb.setAlignment(Qt.AlignVCenter)
+        self.main_lt = QHBoxLayout(self)
+        self.main_lt.addWidget(self.content_w)
+
+        # Size policy
+        self.content_w.setMaximumWidth(1000)
+        labels_vb.setAlignment(Qt.AlignVCenter)
         buttons_vb.setAlignment(Qt.AlignVCenter)
-
         self.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
-    
+
+    def setupStyles(self):
+        ## Labels
+        self.title_l.setFont(fonts.ABOUT_TITLE)
+        self.version_l.setFont(fonts.ABOUT_VERSION)
+        self.credits_l.setFont(fonts.ABOUT_DESC)
+
+        self.title_l.setStyleSheet("padding-bottom: 3px;")
+        self.version_l.setStyleSheet("padding-bottom: 5px;")
+
+        self.title_l.setAlignment(Qt.AlignCenter)
+        self.version_l.setAlignment(Qt.AlignCenter)
+        self.credits_l.setAlignment(Qt.AlignCenter)
+
     def checkForUpdate(self):
         self.update_checker.run()
         self.update_btn.setEnabled(False)
