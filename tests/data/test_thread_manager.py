f843ad78ed2a8ea24bbb32571c85f8ef04fae804
JacobDev1 <65915437+JacobDev1@users.noreply.github.com>
2025-01-22 09:40:20 +0100

    Rewrite unit tests for thread_manager

--- tests/data/test_thread_manager.py
+++ tests/data/test_thread_manager.py
@@ -1,4 +1,5 @@
-from unittest.mock import MagicMock
+from contextlib import ExitStack
+from unittest.mock import MagicMock, patch
 
 import pytest
 from PySide6.QtCore import QThreadPool
@@ -12,39 +13,117 @@ def thread_manager():
     pool.maxThreadCount = MagicMock()
     return ThreadManager(pool)
 
-def test_configure_fixed(thread_manager):
-    thread_manager.configure("JPEG XL", 20, 10)
+@pytest.fixture
+def configure_patches(thread_manager):
+    mocks = {
+        "RAMOptimizer.setEnabled": patch("data.thread_manager.RAMOptimizer.setEnabled"),
+        "RAMOptimizer.isNecessary": patch("data.thread_manager.RAMOptimizer.isNecessary", return_value=False),
+        "RAMOptimizer.setOptimizationRulesStr": patch("data.thread_manager.RAMOptimizer.setOptimizationRulesStr"),
+        "RAMOptimizer.applicableRuleExists": patch("data.thread_manager.RAMOptimizer.applicableRuleExists", return_value=False),
+        "RAMOptimizer.setUsedThreadCount": patch("data.thread_manager.RAMOptimizer.setUsedThreadCount"),
+        "_getBurstThreadPool": patch("data.thread_manager.ThreadManager._getBurstThreadPool", return_value=[]),
+    }
+
+    with ExitStack() as stack:
+        _mocks = {name: stack.enter_context(patcher) for name, patcher in mocks.items()}
+        yield thread_manager, _mocks
+
+def test_configure_no_optimizer_necessary(configure_patches):
+    thread_manager, mocks = configure_patches
+    thread_manager.threads_per_worker = 1
+
+    thread_manager.configure(1, 10, "Static", "", "", "", 1, False, False, False)
+
+    assert thread_manager.threads_per_worker == 1
+    thread_manager.threadpool.setMaxThreadCount.assert_called_once()
+
+def test_configure_burst_threadpool(configure_patches):
+    thread_manager, mocks = configure_patches
+    used_thread_count = 10
+    item_count = 3
+    burst_threadpool = [4, 3, 3]
+    mocks["_getBurstThreadPool"].return_value = burst_threadpool
+
+    thread_manager.configure(item_count, used_thread_count, "Static", "", "", "", 1, False, False, False)
+
+    assert thread_manager.burst_threadpool == burst_threadpool
+    thread_manager.threadpool.setMaxThreadCount.assert_called_once_with(used_thread_count)
+    mocks["_getBurstThreadPool"].assert_called_once_with(item_count, used_thread_count)
+
+def test_configure_optimizer_static(configure_patches):
+    thread_manager, mocks = configure_patches
+    used_thread_count = 10
+    thread_manager.burst_threadpool = [4, 3, 3]
+    mocks["RAMOptimizer.isNecessary"].return_value = True
+
+    thread_manager.configure(1, used_thread_count, "Static", "", "", "", 1, False, False, False)
 
     assert thread_manager.burst_threadpool == []
-    assert thread_manager.getAvailableThreads(0) == 1
-    thread_manager.threadpool.setMaxThreadCount.assert_called_once_with(10)
-
-def test_configure_dynamic(thread_manager):
-    thread_manager.configure("JPEG XL", 5, 11)
-
-    assert thread_manager.burst_threadpool == [3, 2, 2, 2, 2]
-    assert thread_manager.getAvailableThreads(0) == 3
-    assert thread_manager.getAvailableThreads(1) == 2
-    thread_manager.threadpool.setMaxThreadCount.assert_called_once_with(11)
-
-def test_configure_low_ram(thread_manager):
-    thread_manager.configure("JPEG XL", 5, 11, False)
-
-    assert thread_manager.burst_threadpool == []
-    assert thread_manager.getAvailableThreads(0) == 11
+    thread_manager.threads_per_worker == used_thread_count
     thread_manager.threadpool.setMaxThreadCount.assert_called_once_with(1)
 
-def test_getAvailableThreads_burst(thread_manager):
-    thread_manager.configure("JPEG XL", 5, 10)
-    
-    for i in range(5):
-        assert thread_manager.getAvailableThreads(i) == 2
+def test_configure_optimizer_dynamic_args(configure_patches):
+    thread_manager, mocks = configure_patches
+    rules, dst_format, avif_encoder, used_thread_count = "(\"all\", 10, 1)", "AVIF", "SVT-AV1-PSY", 10
+    mocks["RAMOptimizer.applicableRuleExists"].return_value = True
+    mocks["RAMOptimizer.isNecessary"].return_value = True
 
-def test_getAvailableThreads_index_error(thread_manager, caplog):
-    thread_manager.configure("JPEG XL", 5, 10)
+    thread_manager.configure(3, used_thread_count, "Dynamic", rules, dst_format, avif_encoder, 1, False, False, False)
+
+    mocks["RAMOptimizer.setEnabled"].call_count == 2
+    mocks["RAMOptimizer.setEnabled"].call_args_list[1][0][0] == True
+    mocks["RAMOptimizer.setOptimizationRulesStr"].assert_called_once_with(rules)
+    mocks["RAMOptimizer.applicableRuleExists"].assert_called_once_with(dst_format, avif_encoder)
+    mocks["RAMOptimizer.setUsedThreadCount"].assert_called_once_with(used_thread_count)
+    mocks["_getBurstThreadPool"].assert_not_called()
+
+def test_configure_optimizer_dynamic_no_args(configure_patches):
+    thread_manager, mocks = configure_patches
+    mocks["RAMOptimizer.applicableRuleExists"].return_value = False
+    mocks["RAMOptimizer.isNecessary"].return_value = True
+
+    thread_manager.configure(3, 10, "Dynamic", "", "", "", 1, False, False, False)
+
+    mocks["RAMOptimizer.setEnabled"].call_count == 1
+    mocks["RAMOptimizer.setEnabled"].call_args_list[0][0][0] == False
+    mocks["RAMOptimizer.setOptimizationRulesStr"].assert_called_once()
+    mocks["RAMOptimizer.applicableRuleExists"].assert_called_once()
+    mocks["_getBurstThreadPool"].assert_called_once()
+
+def test_configure_optimizer_disabled(configure_patches):
+    thread_manager, mocks = configure_patches
+    mocks["RAMOptimizer.isNecessary"].return_value = True
+
+    thread_manager.configure(1, 1, "Disabled", "", "", "", 1, False, False, False)
+
+    thread_manager._getBurstThreadPool.assert_called_once()
+
+def test_configure_optimizer_unknown(configure_patches, caplog):
+    thread_manager, mocks = configure_patches
+    mocks["RAMOptimizer.isNecessary"].return_value = True
+
+    thread_manager.configure(1, 1, "Unknown", "", "", "", 1, False, False, False)
+
+    assert "Unrecognized ram_optimizer_mode" in caplog.records[0].message
+    thread_manager._getBurstThreadPool.assert_called_once()
+
+def test_getAvailableThreads_burst(thread_manager):
+    burst_threadpool = [3, 2, 2]
+    thread_manager.burst_threadpool = burst_threadpool
+    thread_manager.threads_per_worker = 1
     
-    assert thread_manager.getAvailableThreads(15) == 1
-    assert "IndexError" in caplog.text
+    for i, value in enumerate(burst_threadpool):
+        assert thread_manager.getAvailableThreads(i) == burst_threadpool[i]
+    
+    for i in range(len(burst_threadpool), 10):
+        assert thread_manager.getAvailableThreads(i) == 1
+
+def test_getAvailableThreads_static(thread_manager, caplog):
+    burst_threadpool = []
+    thread_manager.threads_per_worker = 1
+    
+    for i in range(10):
+        assert thread_manager.getAvailableThreads(i) == 1
 
 @pytest.mark.parametrize("workers, cores, expected", [
     (3, 7, [3, 2, 2]),
@@ -58,19 +137,3 @@ def test_getAvailableThreads_index_error(thread_manager, caplog):
 ])
 def test__getBurstThreadPool(workers, cores, expected, thread_manager):
     assert thread_manager._getBurstThreadPool(workers, cores) == expected
-
-@pytest.mark.parametrize("mode, jxl_optimizer, effort, jxl_modular, lossless, intelligent_effort, expected", [
-    ("JPEG", True, 0, False, False, False, True),
-    ("JPEG XL", True, 7, False, False, False, True),
-    ("JPEG XL", True, 8, False, False, False, False),
-    ("JPEG XL", True, 9, False, False, False, False),
-    ("JPEG XL", True, 7, False, True, False, True),
-    ("JPEG XL", True, 9, False, True, False, True),
-    ("JPEG XL", True, 10, False, True, False, False),
-    ("JPEG XL", True, 7, True, False, False, False),
-    ("JPEG XL", False, 10, True, False, False, True),
-])
-def test_isParallelRecommended(mode, jxl_optimizer, effort, jxl_modular, lossless, intelligent_effort, expected, thread_manager):
-    assert thread_manager.isParallelRecommended(
-        mode, jxl_optimizer, effort, jxl_modular, lossless, intelligent_effort
-    ) == expected
