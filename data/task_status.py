91d001e9254ac76c8fef3da89423915262a090d2
JacobDev1 <65915437+JacobDev1@users.noreply.github.com>
2025-02-05 18:44:14 +0100

    Refactor task_status

--- data/task_status.py
+++ data/task_status.py
@@ -1,13 +1,26 @@
-# Module-wide global variable
+from threading import Lock
+from functools import wraps
+
+_lock = Lock()
 
 class Status:
     canceled = False
 
+def synchronized(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        with _lock:
+            return func(*args, **kwargs)
+    return wrapper
+
+@synchronized
 def wasCanceled():
     return Status.canceled
 
+@synchronized
 def cancel():
     Status.canceled = True
 
+@synchronized
 def reset():
     Status.canceled = False
