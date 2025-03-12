5d02f04de5f46c9f3b80823a45bb9c54a0fe78d0
JacobDev1 <65915437+JacobDev1@users.noreply.github.com>
2025-03-12 23:50:20 +0100

    Make small adjustments to conflicts

--- core/conflicts.py
+++ core/conflicts.py
@@ -8,13 +8,18 @@ from core.exceptions import GenericException, FileException
 
 def checkForConflicts(src_ext: str, src_image_path: str, target_format: str, downscaling: bool = False) -> None:
     """
-    Checks for conflicts with animated images. Raises exceptions and returns True If any conflicts occur. 
+    Checks for conflicts with animated images. Raises exceptions and returns True if any conflicts occur. 
     
     Args:
-    - src_ext - extension (without a dot in the beginning and lowercase)
-    - src_image_path - path to source image
-    - target_format - target format
-    - downscaling - is downscaling on
+        - src_ext - extension (without a dot in the beginning and lowercase)
+        - src_image_path - path to source image
+        - target_format - target format
+        - downscaling - is downscaling enabled
+
+    Exceptions:
+        - GenericException - for unsupported transcoding.
+        - FileException - for file-related exceptions.
+
     """
     conflict = True
 
@@ -25,7 +30,7 @@ def checkForConflicts(src_ext: str, src_image_path: str, target_format: str, dow
             conflict = False
         
         if conflict:
-            raise GenericException("CF0", f"{src_ext.upper()} -> {target_format} conversion is not supported")
+            raise GenericException("CF0", f"Transcoding {src_ext.upper()} -> {target_format} is not supported")
 
         if downscaling:
             raise GenericException("CF1", f"Downscaling is not supported for animation")
@@ -34,11 +39,11 @@ def checkForConflicts(src_ext: str, src_image_path: str, target_format: str, dow
         # Multipage images
         page_num, err = getImagePageNum(src_image_path)
         if page_num < 1:
-            raise FileException("CF2", f"Cannot detect the number of pages. {err}")
+            raise FileException("CF2", f"Cannot detect image's page count. {err}")
 
         if page_num > 1:
             if src_ext == "webp":
                 err_msg = "Animated WebP is not supported as input."
             else:
                 err_msg = "Multipage images are not supported."
-            raise FileException("CF3", err_msg)
+            raise GenericException("CF3", err_msg)
