8cfc6b60b672c52b9a2b7bad62071ed1223a27b5
JacobDev1 <65915437+JacobDev1@users.noreply.github.com>
2025-03-30 13:09:11 +0200

    Update input_tab unit tests

--- tests/ui/tabs/test_input_tab.py
+++ tests/ui/tabs/test_input_tab.py
@@ -15,7 +15,11 @@ from ui.lib import WidgetManager
 @pytest.fixture
 def input_tab_widget(app):
     settings = {"sorting_disabled": False}
-    return InputTab(settings)
+    input_tab = InputTab(settings)
+    input_tab.wm = MagicMock(spec=WidgetManager)
+    input_tab.file_view = MagicMock(spec=FileView)
+    input_tab.notify = MagicMock(spec=Notifications)
+    yield input_tab
 
 @pytest.fixture
 def input_tab_patched(input_tab_widget):
@@ -28,16 +32,11 @@ def input_tab_patched(input_tab_widget):
     ]
 
     mocks = {
-        "file_view": patch.object(input_tab_widget, "file_view", MagicMock(spec=FileView)),
-        "QFileDialog": patch("ui.tabs.input_tab.QFileDialog", return_value=mock_file_dialog),
         "_addItems": patch.object(input_tab_widget, "_addItems"),
         "FLATPAK": patch("ui.tabs.input_tab.FLATPAK", False),
-        "wm": patch.object(input_tab_widget, "wm", MagicMock(spec=WidgetManager)),
-        "wm.getVar": patch.object(input_tab_widget.wm, "getVar", return_value=None),
-        "isPathValidStr": patch("ui.tabs.input_tab.isPathValidStr", return_value=True),
         "ALLOWED_INPUT_FILTERS": patch("ui.tabs.input_tab.ALLOWED_INPUT_FILTERS", "All Files (*)"),
         "scanDir": patch("ui.tabs.input_tab.scanDir"),
-        "notify": patch.object(input_tab_widget, "notify", MagicMock(spec=Notifications)),
+        "_createFileDialog": patch.object(input_tab_widget, "_createFileDialog", return_value=mock_file_dialog),
     }
 
     with ExitStack() as stack:
@@ -49,8 +48,21 @@ def input_tab__addItems_patched(input_tab_widget):
     mocks = {
         "notify": patch.object(input_tab_widget, "notify", MagicMock(spec=Notifications)),
         "wm": patch.object(input_tab_widget, "wm", MagicMock(spec=WidgetManager)),
+        "wm.getVar": patch.object(input_tab_widget.wm, "getVar", return_value=None),
         "file_view": patch.object(input_tab_widget, "file_view", MagicMock(spec=FileView)),
         "ALLOWED_INPUT": patch("ui.tabs.input_tab.ALLOWED_INPUT", ["jpg", "png"]),
+    }
+
+    with ExitStack() as stack:
+        _mocks = { name: stack.enter_context(patcher) for name, patcher in mocks.items() }
+        yield input_tab_widget, _mocks
+
+@pytest.fixture
+def input_tab__createFileDialog_patched(input_tab_widget):
+    mocks = {
+        "QFileDialog": patch("ui.tabs.input_tab.QFileDialog", return_value=MagicMock(spec=QFileDialog)),
+        "ALLOWED_INPUT_FILTERS": patch("ui.tabs.input_tab.ALLOWED_INPUT_FILTERS", ["jpg", "png"]),
+        "isPathValidStr": patch("ui.tabs.input_tab.isPathValidStr", return_value=True),
     }
 
     with ExitStack() as stack:
@@ -74,8 +86,8 @@ def test_addFiles_selected_files(input_tab_patched):
         "/tmp/image_0.jpg",
         "/tmp/image_1.jpg",
     ]
-    mocks["QFileDialog"].return_value.exec.return_value = True
-    mocks["QFileDialog"].return_value.selectedFiles.return_value = sample_files
+    mocks["_createFileDialog"].return_value.exec.return_value = True
+    mocks["_createFileDialog"].return_value.selectedFiles.return_value = sample_files
 
     input_tab.addFiles()
 
@@ -86,48 +98,14 @@ def test_addFiles_selected_files(input_tab_patched):
 
 def test_addFiles_no_selected_files(input_tab_patched):
     input_tab, mocks = input_tab_patched
-    mocks["QFileDialog"].return_value.exec.return_value = False
+    mocks["_createFileDialog"].return_value.exec.return_value = False
     input_tab.addFiles()
     input_tab._addItems.assert_not_called()
 
 def test_addFiles_file_dlg_setup(input_tab_patched):
     input_tab, mocks = input_tab_patched
     input_tab.addFiles()
-    mocks["QFileDialog"].assert_called_once_with(
-        input_tab,
-        "Add Images",
-        ANY
-    )
-    mocks["QFileDialog"].return_value.setFileMode.assert_called_once_with(mocks["QFileDialog"].ExistingFiles)
-    mocks["QFileDialog"].return_value.setNameFilters.assert_called_once_with(mocks["ALLOWED_INPUT_FILTERS"])
-
-def test_addFiles_file_dlg_no_last_dir(input_tab_patched):
-    input_tab, mocks = input_tab_patched
-    input_tab.wm.getVar.return_value = None
-    input_tab.addFiles()
-    mocks["QFileDialog"].assert_called_once_with(
-        input_tab,
-        "Add Images",
-        QDir.homePath()
-    )
-
-def test_addFiles_file_dlg_load_last_dir(input_tab_patched):
-    input_tab, mocks = input_tab_patched
-    last_dir = "/tmp"
-    input_tab.wm.getVar.return_value = last_dir
-    input_tab.addFiles()
-    mocks["QFileDialog"].assert_called_once_with(
-        input_tab,
-        "Add Images",
-        last_dir
-    )
-
-def test_addFiles_file_dlg_save_last_dir(input_tab_patched):
-    input_tab, mocks = input_tab_patched
-    used_dir = "/tmp"
-    mocks["QFileDialog"].return_value.directory.return_value.absolutePath.return_value = used_dir
-    input_tab.addFiles()
-    input_tab.wm.setVar.assert_called_once_with("add_files_last_dir", used_dir)
+    mocks["_createFileDialog"].assert_called_once_with("files", "Add Images")
 
 def test_addFiles_files_selected(input_tab_patched):
     input_tab, mocks = input_tab_patched
@@ -135,7 +113,7 @@ def test_addFiles_files_selected(input_tab_patched):
         "/tmp/image_0.jpg",
         "/tmp/image_1.jpg",
     ]
-    mocks["QFileDialog"].return_value.selectedFiles.return_value = sample_files
+    mocks["_createFileDialog"].return_value.selectedFiles.return_value = sample_files
     input_tab.addFiles()
     for n, sample_file in enumerate(sample_files):
         assert input_tab._addItems.call_args[0][0][n][0] == Path(sample_file)
@@ -143,7 +121,7 @@ def test_addFiles_files_selected(input_tab_patched):
 
 def test_addFiles_no_files_selected(input_tab_patched):
     input_tab, mocks = input_tab_patched
-    mocks["QFileDialog"].return_value.exec.return_value = 0
+    mocks["_createFileDialog"].return_value.exec.return_value = 0
     input_tab.addFiles()
     input_tab.wm.setVar.assert_not_called()
 
@@ -151,23 +129,23 @@ def test_addFiles_no_flatpak(input_tab_patched):
     input_tab, mocks = input_tab_patched
     with patch("ui.tabs.input_tab.FLATPAK", False):
         input_tab.addFiles()
-        mocks["notify"].notify.assert_not_called()
+        input_tab.notify.notify.assert_not_called()
 
 def test_addFiles_flatpak_has_permission(input_tab_patched):
     input_tab, mocks = input_tab_patched
-    mocks["QFileDialog"].return_value.selectedFiles.return_value = ["/tmp/image.jpg"]
+    mocks["_createFileDialog"].return_value.selectedFiles.return_value = ["/tmp/image.jpg"]
     with patch("ui.tabs.input_tab.FLATPAK", True):
         input_tab.addFiles()
-        mocks["notify"].notify.assert_not_called()
+        input_tab.notify.notify.assert_not_called()
 
 def test_addFiles_flatpak_no_permissions(input_tab_patched):
     input_tab, mocks = input_tab_patched
-    mocks["QFileDialog"].return_value.selectedFiles.return_value = ["/run/user/1000/doc/123456789/Pictures/image.jpg"]
+    mocks["_createFileDialog"].return_value.selectedFiles.return_value = ["/run/user/1000/doc/123456789/Pictures/image.jpg"]
     with patch("ui.tabs.input_tab.FLATPAK", True):
         input_tab.addFiles()
-        mocks["notify"].notify.assert_called_once()
-        assert "Flatpak" in mocks["notify"].notify.call_args[0][0]
-        assert "add filesystem permissions" in mocks["notify"].notify.call_args[0][1]
+        input_tab.notify.notify.assert_called_once()
+        assert "Flatpak" in input_tab.notify.notify.call_args[0][0]
+        assert "add filesystem permissions" in input_tab.notify.notify.call_args[0][1]
 
 def test_addFiles_happy_path(input_tab_patched):
     input_tab, mocks = input_tab_patched
@@ -175,23 +153,13 @@ def test_addFiles_happy_path(input_tab_patched):
         "/tmp/image_0.jpg",
         "/tmp/image_1.jpg",
     ]
-    used_dir = "/tmp"
-    input_tab.wm.getVar.return_value = None
-    mocks["QFileDialog"].return_value.exec.return_value = True
-    mocks["QFileDialog"].return_value.directory.return_value.absolutePath.return_value = used_dir
-    mocks["QFileDialog"].return_value.selectedFiles.return_value = sample_files
+    mocks["_createFileDialog"].return_value.exec.return_value = True
+    mocks["_createFileDialog"].return_value.selectedFiles.return_value = sample_files
 
     input_tab.addFiles()
 
-    mocks["QFileDialog"].assert_called_once_with(
-        input_tab,
-        "Add Images",
-        QDir.homePath()
-    )
-    mocks["QFileDialog"].return_value.setFileMode.assert_called_once_with(mocks["QFileDialog"].ExistingFiles)
-    mocks["QFileDialog"].return_value.setNameFilters.assert_called_once_with(mocks["ALLOWED_INPUT_FILTERS"])
-    mocks["QFileDialog"].return_value.exec.assert_called_once()
-    input_tab.wm.setVar.assert_called_once_with("add_files_last_dir", used_dir)
+    mocks["_createFileDialog"].assert_called_once_with("files", "Add Images")
+    mocks["_createFileDialog"].return_value.exec.assert_called_once()
     for n, sample_file in enumerate(sample_files):
         assert input_tab._addItems.call_args[0][0][n][0] == Path(sample_file)
         assert input_tab._addItems.call_args[0][0][n][1] == Path(sample_file).parent
@@ -203,8 +171,8 @@ def test_addFolder_selected_folder(input_tab_patched):
         "/tmp/image_0.jpg",
         "/tmp/image_1.jpg",
     ]
-    mocks["QFileDialog"].return_value.exec.return_value = True
-    mocks["QFileDialog"].return_value.selectedFiles.return_value = [sample_folder]
+    mocks["_createFileDialog"].return_value.exec.return_value = True
+    mocks["_createFileDialog"].return_value.selectedFiles.return_value = [sample_folder]
     mocks["scanDir"].return_value = sample_files
 
     input_tab.addFolder()
@@ -221,79 +189,37 @@ def test_addFolder_selected_folder_not_found(input_tab_patched):
     input_tab.addFolder()
 
     input_tab._addItems.assert_not_called()
-    mocks["notify"].notify.assert_called_once()
-    assert "Error" in mocks["notify"].notify.call_args[0][0]
-    assert "not found" in mocks["notify"].notify.call_args[0][1]
+    input_tab.notify.notify.assert_called_once()
+    assert "Error" in input_tab.notify.notify.call_args[0][0]
+    assert "not found" in input_tab.notify.notify.call_args[0][1]
 
 def test_addFolder_empty_selection(input_tab_patched):
     input_tab, mocks = input_tab_patched
-    mocks["QFileDialog"].return_value.exec.return_value = False
+    mocks["_createFileDialog"].return_value.exec.return_value = False
     input_tab.addFolder()
     input_tab._addItems.assert_not_called()
 
 def test_addFolder_file_dlg_setup(input_tab_patched):
     input_tab, mocks = input_tab_patched
     input_tab.addFolder()
-    mocks["QFileDialog"].assert_called_once_with(
-        input_tab,
-        "Add Images from a Folder",
-        ANY
-    )
-    mocks["QFileDialog"].return_value.setFileMode.assert_called_once_with(mocks["QFileDialog"].Directory)
+    mocks["_createFileDialog"].assert_called_once_with("folder", "Add Images from a Folder")
 
-def test_addFolder_file_dlg_no_last_dir(input_tab_patched):
-    input_tab, mocks = input_tab_patched
-    input_tab.wm.getVar.return_value = None
-    input_tab.addFolder()
-    mocks["QFileDialog"].assert_called_once_with(
-        input_tab,
-        "Add Images from a Folder",
-        QDir.homePath()
-    )
-
-def test_addFolder_file_dlg_load_last_dir(input_tab_patched):
-    input_tab, mocks = input_tab_patched
-    last_dir = "/tmp"
-    input_tab.wm.getVar.return_value = last_dir
-    input_tab.addFolder()
-    mocks["QFileDialog"].assert_called_once_with(
-        input_tab,
-        "Add Images from a Folder",
-        last_dir
-    )
-
-def test_addFolder_file_dlg_save_last_dir(input_tab_patched):
-    input_tab, mocks = input_tab_patched
-    used_dir = "/tmp"
-    mocks["QFileDialog"].return_value.directory.return_value.absolutePath.return_value = used_dir
-    input_tab.addFolder()
-    input_tab.wm.setVar.assert_called_once_with("add_folder_last_dir", used_dir)
-
-def test_addFiles_happy_path(input_tab_patched):
+def test_addFolder_happy_path(input_tab_patched):
     input_tab, mocks = input_tab_patched
     sample_files = [
         "/tmp/image_0.jpg",
         "/tmp/image_1.jpg",
     ]
-    used_dir = "/tmp"
     sample_folder = "/tmp/image_0.jpg"
     
-    input_tab.wm.getVar.return_value = None
-    mocks["QFileDialog"].return_value.exec.return_value = True
-    mocks["QFileDialog"].return_value.directory.return_value.absolutePath.return_value = used_dir
-    mocks["QFileDialog"].return_value.selectedFiles.return_value = [sample_folder]
+    mocks["_createFileDialog"].return_value.exec.return_value = True
+    mocks["_createFileDialog"].return_value.selectedFiles.return_value = [sample_folder]
     mocks["scanDir"].return_value = sample_files
 
     input_tab.addFolder()
 
-    mocks["QFileDialog"].assert_called_once_with(
-        input_tab,
-        "Add Images from a Folder",
-        QDir.homePath()
-    )
-    mocks["QFileDialog"].return_value.setFileMode.assert_called_once_with(mocks["QFileDialog"].Directory)
-    mocks["QFileDialog"].return_value.exec.assert_called_once()
-    input_tab.wm.setVar.assert_called_once_with("add_folder_last_dir", used_dir)
+    mocks["_createFileDialog"].assert_called_once_with("folder", "Add Images from a Folder")
+    mocks["_createFileDialog"].return_value.exec.assert_called_once()
     for n, sample_file in enumerate(sample_files):
         assert input_tab._addItems.call_args[0][0][n][0] == Path(sample_file)
         assert input_tab._addItems.call_args[0][0][n][1] == Path(sample_folder)
@@ -312,6 +238,46 @@ def test_saveState(input_tab_patched):
     input_tab, mocks = input_tab_patched
     input_tab.saveState()
     input_tab.wm.saveState.assert_called_once()
+
+def test__createFileDialog_unsupported_mode(input_tab__createFileDialog_patched):
+    input_tab, mocks = input_tab__createFileDialog_patched
+    assert input_tab._createFileDialog("unsupported", "caption") is None
+
+@pytest.mark.parametrize("mode, expected_key", [
+    ("files", "add_files_last_dir"),
+    ("folder", "add_folder_last_dir"),
+])
+def test__createFileDialog_supported_modes(mode, expected_key, input_tab__createFileDialog_patched):
+    input_tab, mocks = input_tab__createFileDialog_patched
+    assert input_tab._createFileDialog(mode, "caption")
+    input_tab.wm.getVar.assert_called_once_with(expected_key)
+
+def test__createFileDialog_no_saved_data(input_tab__createFileDialog_patched):
+    input_tab, mocks = input_tab__createFileDialog_patched
+    input_tab.wm.getVar.return_value = None
+
+    input_tab._createFileDialog("files", "caption")
+    
+    mocks["QFileDialog"].assert_called_once_with(
+        input_tab,
+        "caption",
+        QDir.homePath()
+    )
+
+def test__createFileDialog_mode_files_setup(input_tab__createFileDialog_patched):
+    input_tab, mocks = input_tab__createFileDialog_patched
+
+    input_tab._createFileDialog("files", "caption")
+
+    mocks["QFileDialog"].return_value.setFileMode.assert_called_once_with(mocks["QFileDialog"].ExistingFiles)
+    mocks["QFileDialog"].return_value.setNameFilters.assert_called_once_with(mocks["ALLOWED_INPUT_FILTERS"])
+
+def test__createFileDialog_mode_folder_setup(input_tab__createFileDialog_patched):
+    input_tab, mocks = input_tab__createFileDialog_patched
+
+    input_tab._createFileDialog("folder", "caption")
+
+    mocks["QFileDialog"].return_value.setFileMode.assert_called_once_with(mocks["QFileDialog"].Directory)
 
 def test__addItems_no_items(input_tab__addItems_patched):
     input_tab, mocks = input_tab__addItems_patched
