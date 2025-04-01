824d925489321fd7a9b71da3c129030ab2615bf6
JacobDev1 <65915437+JacobDev1@users.noreply.github.com>
2025-04-01 19:00:53 +0200

    Bump version

--- data/constants.py
+++ data/constants.py
@@ -6,7 +6,7 @@ import logging
 from data.utils import removeDuplicatesHashable, listToFilter, isRunningInFlatpak
 from data.config_manager import ConfigManager
 
-VERSION = "1.2.0"
+VERSION = "1.2.1"
 UPDATE_CHECKER_VER_FILE_URL = "https://codepoems.eu/downloads/xl-converter/version.json"   # Used by UpdateChecker; example in misc/version.json
 UPDATE_CHECKER_ENABLED = True
 
