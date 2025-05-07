af4a26ede91c7a848102a1e16fa52074b9ebb4ef
JacobDev1 <65915437+JacobDev1@users.noreply.github.com>
2025-05-07 12:07:04 +0200

    Refactor ExifTool download script

--- misc/build_scripts/windows/exiftool.sh
+++ misc/build_scripts/windows/exiftool.sh
@@ -2,17 +2,37 @@
 # Compatible with Linux Bash and MSYS2
 
 EXIFTOOL_TAG="13.25"
-OUTPUT_DIR="./bin/win/exiftool"
+
+RUN_DIR=$(pwd)
+OUTPUT_DIR="${RUN_DIR}/bin/win/exiftool"
+TMP_DIR=$(mktemp -d)
 
 set -e
 
-# Requires: wget, p7zip-full
+# Check pre-requisites
+for cmd in wget 7z; do
+    if ! command -v "$cmd" &> /dev/null; then
+        echo "Error: Command \"${cmd}\" is not available."
+        echo "Install it and run the script again."
+        exit 1
+    fi
+done
 
-mkdir "$OUTPUT_DIR"
-cd "$OUTPUT_DIR"
+# Prepare
+rm -rf "$TMP_DIR"
+mkdir -p "$TMP_DIR"
+cd "$TMP_DIR"
 wget "https://exiftool.org/exiftool-${EXIFTOOL_TAG}_64.zip" -O exiftool.zip
 7z x exiftool.zip
 cd "exiftool-${EXIFTOOL_TAG}_64"
 mv "exiftool(-k).exe" exiftool.exe
 rm -f ./exiftool_files/Licenses_Strawberry_Perl.zip     # 0.5 MB saved
 rm -f ./README.txt
+
+# Move
+cd "$RUN_DIR"
+mkdir -p "$OUTPUT_DIR"
+mv "${TMP_DIR}"/exiftool-"${EXIFTOOL_TAG}"_64/* "${OUTPUT_DIR}"
+
+# Clean up
+rm -rf "$TMP_DIR"
